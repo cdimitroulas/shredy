124e2bd0a8167696b2ad79831a44b0de7e0086e7
Petya Nikolova <petia_182@yahoo.com>
2017-12-03 19:38:14 +0000

    log database connection status

--- src/mongoose.js
+++ src/mongoose.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose');
 
 module.exports = function (app) {
+  mongoose.Promise = global.Promise;
+
   mongoose.connect(app.get('mongodb'), {
     useMongoClient: true
-  });
-  mongoose.Promise = global.Promise;
+  })
+    .then(() => {
+      console.log(`Connected to db at ${app.get('mongodb')}`)
+    })
+    .catch(error => {
+      console.log(`Error connecting to mongodb: ${error.message}`)
+    });
+
 
   app.set('mongooseClient', mongoose);
 };
